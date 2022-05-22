93e5b58ab7c796c4a9ff1214719a952529f6aca3
Mohamed Salem <m.salim@it-cores.com>
2022-05-22 06:45:59 +0200

    footer

--- src/app/modules/electronic-council/components/user-sidemenu/user-sidemenu.component.ts
+++ src/app/modules/electronic-council/components/user-sidemenu/user-sidemenu.component.ts
@@ -5,26 +5,21 @@ import { UserService } from '@shared/services/user.service';
 @Component({
   selector: 'app-user-sidemenu',
   templateUrl: './user-sidemenu.component.html',
-  styleUrls: ['./user-sidemenu.component.scss']
+  styleUrls: ['./user-sidemenu.component.scss'],
 })
 export class UserSidemenuComponent implements OnInit {
+  userModel: UserModel;
 
-  userModel : UserModel
-
-  constructor(private userService : UserService) { 
+  constructor(private userService: UserService) {
     this.userModel = new UserModel();
-    
-    debugger
-    this.userModel.displayName = this.userService.currentUser?.displayName
-    this.userModel.email = this.userService.currentUser?.email
-    this.userModel.address = this.userService.currentUser?.address
-    this.userModel.phoneNumber = this.userService.currentUser?.phoneNumber
 
-    console.log(typeof(this.userService.currentUser))
+    this.userModel.displayName = this.userService.currentUser?.displayName;
+    this.userModel.email = this.userService.currentUser?.email;
+    this.userModel.address = this.userService.currentUser?.address;
+    this.userModel.phoneNumber = this.userService.currentUser?.phoneNumber;
+
+    console.log(typeof this.userService.currentUser);
   }
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
