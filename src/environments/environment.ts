9158bef21ee016690fa28e1ed63af6be295d26aa
mohamed.hussien <mohamed.hussien@excelsystems-eg.com>
2022-06-05 02:03:46 +0200

    edit port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl : 'http://localhost:5162/api',
+  apiUrl: 'http://localhost:5162/api',
   imagePathURL: 'http://localhost:5162/uploads/',
-  adminUrl : 'https://localhost:5001',
+  adminUrl: 'http://localhost:5000',
 };
 
 /*
