0f5ffb3ac8388bfafc16cb3f0bed8a1df795138f
Mahmoud Ali <mahmoudali10010@gmail.com>
2022-06-20 01:52:53 +0200

    made rate service function

--- src/app/modules/electronic-council/components/rate-service/rate-service.component.ts
+++ src/app/modules/electronic-council/components/rate-service/rate-service.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rate-service',
   templateUrl: './rate-service.component.html',
   styleUrls: ['./rate-service.component.scss']
 })
-export class RateServiceComponent implements OnInit {
+export class RateServiceComponent implements OnInit , OnDestroy {
 
-  constructor() { }
+  @Output() OnSubmit = new EventEmitter<number>();
+
+  @Output() OnClose = new EventEmitter();
+
+
+  rate : number = 0
+
+  constructor() {
+
+   }
+  ngOnDestroy(): void {
+    this.rate = 1
+    }
 
   ngOnInit(): void {
+    this.rate = 0
+
   }
+
+  rateService(starsCount : number){
+    this.rate = starsCount;
+  }
+
+  onnModalClose() {
+    this.OnClose.emit();
+    this.rate = 0
+
+  }
+
+  onnModalSubmit() {
+    this.OnSubmit.emit(this.rate);
+    this.rate = 0
+
+  }
+
 
 }
--- src/app/modules/electronic-council/components/user-sidemenu/user-sidemenu.component.ts
+++ src/app/modules/electronic-council/components/user-sidemenu/user-sidemenu.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RateService } from '@shared/models/rate-service';
 import { UserModel } from '@shared/models/user-model';
+import { RateServiceServiceService } from '@shared/services/rate-service-service.service';
 import { UserService } from '@shared/services/user.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-user-sidemenu',
@@ -10,19 +13,52 @@ import { UserService } from '@shared/services/user.service';
 export class UserSidemenuComponent implements OnInit {
   userModel: UserModel;
 
-  constructor(private userService: UserService) {
+  showRateServiceModal : boolean = false
+
+  constructor(private _userService: UserService,
+    private _rateService : RateServiceServiceService,
+    private messageService: MessageService,
+    ) {
     this.userModel = new UserModel();
 
-    
-    this.userModel.Name = this.userService.currentUser?.Name
-    this.userModel.Email = this.userService.currentUser?.Email
-    this.userModel.Address = this.userService.currentUser?.Address
-    this.userModel.PhoneNumber = this.userService.currentUser?.PhoneNumber
-    this.userModel.Role = this.userService.currentUser?.Role;
-    this.userModel.IsAdmin = this.userService.currentUser?.IsAdmin;
 
-    console.log(typeof(this.userService.currentUser))
+    this.userModel.Name = this._userService.currentUser?.Name
+    this.userModel.Email = this._userService.currentUser?.Email
+    this.userModel.Address = this._userService.currentUser?.Address
+    this.userModel.PhoneNumber = this._userService.currentUser?.PhoneNumber
+    this.userModel.Role = this._userService.currentUser?.Role;
+    this.userModel.IsAdmin = this._userService.currentUser?.IsAdmin;
+
+    console.log(typeof(this._userService.currentUser))
   }
 
   ngOnInit(): void {}
+
+  closeModal(){
+    this.showRateServiceModal = false;
+  }
+
+  submitModal(starsCount : number){
+    console.log('submit')
+    console.log(starsCount)
+
+
+    let serviceRate : any = {StarsCount : starsCount}
+
+    this._rateService.addRateService(serviceRate).subscribe(
+      (result : any) => {
+        if(result.IsSuccess == true){
+          this.messageService.add({severity:'success', summary: 'تم الحفظ', detail: 'تك حفظ تقييمك بنجاح'});
+        }
+      },
+      (err) => {
+        this.messageService.add({severity:'error', summary: 'خطأ', detail: 'خطأ'});
+
+      }
+    )
+    this.showRateServiceModal = false;
+  }
+
+  close(event : any){
+  }
 }
